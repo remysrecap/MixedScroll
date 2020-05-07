5b7453d2cd1a346b16d157497e967139d95d35ce
remysrecap <mikyrembach@gmail.com>
2020-05-07 12:02:20 -0400

    Initial commit

---
+++ scrollinteraction.js
@@ -0,0 +1,24 @@
+scrollVert();
+var scrollLeft=0;
+
+function scrollVert() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 40);
+    e.preventDefault();
+    setTimeout(function() {
+      if ($(window).scrollTop() + $(window).height() == $(document).height())
+        scrollHoriz();
+    }, 0)
+
+  });
+}
+function scrollHoriz() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollLeft -= (delta * 40);
+    e.preventDefault();
+    scrollLeft=this.scrollLeft
+    setTimeout(function() {
+      if (scrollLeft == 0) scrollVert();
+    }, 0)
+  });
+}
